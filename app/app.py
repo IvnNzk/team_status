fb076e912eebf2a7a6cf94b459225e359f45d8b0
Ivan Oschepkov <ivan@oschepkov.ru>
2020-04-21 22:18:05 +0300

    update app

--- app/app.py
+++ app/app.py
@@ -1,28 +1,36 @@
 from sanic import Sanic
-from sanic.response import html
-
+from sanic.views import HTTPMethodView
+from sanic.response import html, json
+from sanic_session import Session, InMemorySessionInterface
 from jinja2 import Environment, PackageLoader
+from sanic_jinja2 import SanicJinja2
+
+app = Sanic(name='app')
+jinja = SanicJinja2(app)
+
+session = Session(app, interface=InMemorySessionInterface())
 
 env = Environment(loader=PackageLoader('app', 'templates'))
-
-app = Sanic()
 
 app.static('/static/', './static/')
 
 
-@app.route('/')
-async def index(request):
-    template = env.get_template('index.html')
-    html_content = template.render()
-    return html(html_content)
+class DashboardView(HTTPMethodView):
+    async def get(self, request):
+        persons = [{'name': 'Ivan Oschepkov', 'status': 'working on <a href="#">#7858389</a>'},]
+        return jinja.render('index.html.j2', request, team_members=persons)
+
+class LoginView(HTTPMethodView):
+    async def get(self, request):
+        return jinja.render('login.html.j2', request)
+
+    async def post(self, request):
+        return json({'form_data': request.form})
 
 
-@app.route('/login')
-async def login(request):
-    template = env.get_template('login.html')
-    html_content = template.render()
-    return html(html_content)
+app.add_route(DashboardView.as_view(), '/')
+app.add_route(LoginView.as_view(), '/login')
 
 
-app.run(host='0.0.0.0', port=8000)
+app.run(host='0.0.0.0', port=8000, debug=True)
 
